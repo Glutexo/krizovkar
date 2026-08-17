a26b0a32a377d671018ad85535960bcc16c70bcd
Glutexo <glutexo@icloud.com>
2026-08-17 21:46:42 +0200

    style: zúž dokumentové okno na 660 pixelů

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -875,8 +875,8 @@ class GuiTest(unittest.TestCase):
 
         window._configure_window()
 
-        window.root.geometry.assert_called_once_with("720x850")
-        window.root.minsize.assert_called_once_with(720, 700)
+        window.root.geometry.assert_called_once_with("660x850")
+        window.root.minsize.assert_called_once_with(660, 700)
         window.root.bind.assert_called_once_with(
             "<FocusIn>",
             window._document_focus_in,
